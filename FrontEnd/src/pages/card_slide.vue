02149ca3cd6b9b52d69d2c21c6fc1a28c69855c0
adam <112017122+Adammuqri03@users.noreply.github.com>
2024-01-31 21:48:51 +0800

    card

--- FrontEnd/src/pages/card_slide.vue
+++ FrontEnd/src/pages/card_slide.vue
@@ -1,3 +1,42 @@
 <template>
-  hi
+  <div id="app">
+		<GameCardsStack
+			:cards="visibleCards"
+			@cardAccepted="handleCardAccepted"
+			@cardRejected="handleCardRejected"
+			@cardSkipped="handleCardSkipped"
+			@hideCard="removeCardFromDeck"
+		/>
+  </div>
 </template>
+
+<script>
+import GameCardsStack from "../components/GameCardsStack.vue";
+
+export default {
+  components: {
+    GameCardsStack
+  },
+
+  data() {
+    return {
+      visibleCards: ["Test", "Hehe", "Webpack"]
+    };
+  },
+
+  methods: {
+    handleCardAccepted() {
+      console.log("handleCardAccepted");
+    },
+    handleCardRejected() {
+      console.log("handleCardRejected");
+    },
+    handleCardSkipped() {
+      console.log("handleCardSkipped");
+    },
+    removeCardFromDeck() {
+      this.visibleCards.shift();
+    }
+  }
+};
+</script>
