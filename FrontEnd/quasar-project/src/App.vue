6711bd1eae56159d96fd1f8a75ddb07908692950
adam <112017122+Adammuqri03@users.noreply.github.com>
2024-01-31 16:45:33 +0800

    update

--- FrontEnd/quasar-project/src/App.vue
+++ FrontEnd/quasar-project/src/App.vue
@@ -1,11 +1,52 @@
 <template>
-  <router-view />
+  <div id="app">
+		<GameCardsStack
+			:cards="visibleCards"
+			@cardAccepted="handleCardAccepted"
+			@cardRejected="handleCardRejected"
+			@cardSkipped="handleCardSkipped"
+			@hideCard="removeCardFromDeck"
+		/>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import GameCardsStack from "./components/GameCardsStack";
 
-export default defineComponent({
-  name: 'App'
-})
+export default {
+  name: "App",
+  components: {
+    GameCardsStack
+  },
+
+  data() {
+    return {
+      visibleCards: ["Test", "Hehe", "Webpack"]
+    };
+  },
+
+  methods: {
+    handleCardAccepted() {
+      console.log("handleCardAccepted");
+    },
+    handleCardRejected() {
+      console.log("handleCardRejected");
+    },
+    handleCardSkipped() {
+      console.log("handleCardSkipped");
+    },
+    removeCardFromDeck() {
+      this.visibleCards.shift();
+    }
+  }
+};
 </script>
+
+<style lang="scss">
+@import "./styles/mixins.scss";
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  text-align: center;
+}
+</style>
--- FrontEnd/quasar-project/src/router/routes.js
+++ FrontEnd/quasar-project/src/router/routes.js
@@ -4,7 +4,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/card_slide.vue') },
+      { path: '/card_slide', component: () => import('pages/card_slide.vue') }
     ]
   },
 
