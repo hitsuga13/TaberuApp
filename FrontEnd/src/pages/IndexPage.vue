c8d1ea8865f4d8162eb5149f06c97dec310cee25
hitsuga13 <126145510+hitsuga13@users.noreply.github.com>
2024-02-01 13:02:06 +0800

    update indexPage

--- FrontEnd/src/pages/IndexPage.vue
+++ FrontEnd/src/pages/IndexPage.vue
@@ -4,20 +4,3 @@
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from "vue";
-import { globalModule } from "src/stores/globalmodule";
-export default {
-  setup() {
-    const store = globalModule();
-    console.log(store);
-    // Access state and actions from your store
-    // For example: store.state.myProperty, store.actions.myAction()
-
-    return {
-      store,
-    };
-  },
-  name: "IndexPage",
-};
-</script>
